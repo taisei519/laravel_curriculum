43a759e82fc872dc072a05f6ff92e0d5a721e841
taisei519 <y.taisei519@icloud.com>
2021-10-29 02:01:33 +0900

    [add]記事編集

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+       
+    </head>
+    <body>
+        <h1>Blog Name</h1>
+        <form action="/posts/{{ $post->id }}" method="POST">
+            {{ csrf_field() }}
+            @method('PUT')
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value="{{ $post->title }}"/>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日もお疲れ様でした。">{{ $post->body }}</textarea>
+            </div>
+            <input type="submit" value="update"/>
+        </form>
+        <div class="back">[<a href="/posts/{{ $post->id }}">back</a>]</div>
+    </body>
+</html>
